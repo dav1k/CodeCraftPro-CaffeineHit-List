8c1afbac203944787b3ba77b5dbafc0a630d9a1d
dav1k <dave.klausz@icloud.com>
2016-01-16 23:00:39 -0800

    Implemented pull-to-refresh with ion-refresher component.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,4 +6,13 @@ var app = angular.module('caffeinehit.controllers', []);
 
 app.controller('YelpCtrl', function ($scope, YelpService) {
   $scope.yelp = YelpService;
+
+  // doRefresh function
+  $scope.doRefresh = function () {
+    if (!$scope.yelp.isLoading) {
+      $scope.yelp.refresh().then(function () {
+        $scope.$broadcast('scroll.refreshComplete');
+      });
+    }
+  };
 });
